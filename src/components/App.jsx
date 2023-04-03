e9f917f9108e1467b511f3c1a299aa023b8382b4
Alexandr-Styopin <styopin.alexandr@gmail.com>
2023-04-03 09:57:00 +0200

    hw5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+async function fethTrending() {
+  const API_KEY = 'aa1d2a2b8177ac3b5ad3698898d78d80';
+
+  return await fetch(
+    `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&page=1`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
+}
+fethTrending().then(data => console.log(data));
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>React homework template</div>;
 };
